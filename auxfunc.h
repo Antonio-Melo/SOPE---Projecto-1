13846d79c06fe37f2198b45d65df6038d256b11d
Antonio <happyd3nt@gmail.com>
2016-04-17 13:06:41 +0100

    added auxfunc's

---
+++ auxfunc.h
@@ -0,0 +1,7 @@
+#ifndef AUXFUNC_H
+#define AUXFUNC_H
+
+int cmpFileTime (const void* elem1, const void* elem2);
+int cmpFiles (const file_info* elem1, const file_info* elem2);
+
+#endif
--- rmdup.h
+++ rmdup.h
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "auxfunc.h"
 
 #define NAME_LENGTH 256
 #define TRUE 1
@@ -21,8 +22,5 @@ typedef struct file_info {
 	unsigned long time_cg; 		// Time last modified
 	unsigned long protection; 	// Permissions
 } file_info;
-
-int cmpFileTime (const void* elem1, const void* elem2);
-int cmpFiles (const file_info* elem1, const file_info* elem2);
 
 #endif
